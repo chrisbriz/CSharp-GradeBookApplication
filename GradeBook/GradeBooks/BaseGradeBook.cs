6531b2d9e99878ee0b1cd6cc2c22dd61c08a19c7
chrisbriz <christopher.brizet@outlook.com>
2019-01-09 13:03:59 +0100

    weighted and gps calc

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,11 +14,13 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -110,15 +112,30 @@ namespace GradeBook.GradeBooks
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    if (IsWeighted)
+                        return 5;
+                    else
+                        return 4;
                 case 'B':
-                    return 3;
+                    if (IsWeighted)
+                        return 4;
+                    else
+                        return 3;
                 case 'C':
-                    return 2;
+                    if (IsWeighted)
+                        return 3;
+                    else
+                        return 2;
                 case 'D':
-                    return 1;
+                    if (IsWeighted)
+                        return 2;
+                    else
+                        return 1;
                 case 'F':
-                    return 0;
+                    if (IsWeighted)
+                        return 1;
+                    else
+                        return 0;
             }
             return 0;
         }
@@ -264,7 +281,7 @@ namespace GradeBook.GradeBooks
                              from type in assembly.GetTypes()
                              where type.FullName == "GradeBook.GradeBooks.StandardGradeBook"
                              select type).FirstOrDefault();
-            
+
             return JsonConvert.DeserializeObject(json, gradebook);
         }
     }
